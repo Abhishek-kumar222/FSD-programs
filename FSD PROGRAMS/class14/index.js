df2d270e33c6d0f4fcaab9ab67c10b8570f1cc4e
Abhishek Choudhary <choudharyabhishek40739>
2025-01-16 18:12:10 +0530

    FSD

---
+++ FSD PROGRAMS/class14/index.js
@@ -0,0 +1,62 @@
+console.log(1 + 2 + '3'); //'33'
+console.log(false + false); // => 0
+console.log(true + 1);  // => 2
+ console.log('5' - '3'); // => 2
+console.log('5' - 3); // => 2
+console.log('5' * '2');  // =>  10
+console.log('5' / '2'); // => 2.5 
+console.log('5' + '3'); // => 53 
+console.log(null == undefined);  // => true
+console.log(null === undefined); //=>  false
+console.log(null + 1);   //=> 1
+console.log(undefined + 1); // NaN
+console.log(!!'');  //=> false
+
+console.log(!!'hello');   //=> true   
+console.log(!!0);// => false
+console.log(!!1);// => true
+console.log(!!undefined); // => false
+console.log(!!null);//=> false
+// let sum= function(){
+
+// }
+// sum()
+// console.log(typeof(sum));
+// console.log(typeof(null));
+// 'hello'+1
+// let a=5
+// Object
+// Array
+// function
+// let arr=[1,2,3,4,56,'false']
+// console.log([]==[]);// =>   false 
+// let arr=[]
+// let arr1=arr
+// console.log(arr1==arr);
+// let arr=[1,2,3,4]
+// let arr1=arr 
+// arr1.push(5) 
+// console.log(arr,'arr'); //[1,2,3,4]|| [1,2,3,4,5]
+// console.log(arr1,'arr1');//[1,2,3,4,5] ||[0,1,2,3,4]
+// console.log();
+// new Woman()
+
+// let student={
+// id:1,
+// name:"modiji",
+// age:74,
+// isPass:true
+// }
+
+// console.log(student.age);
+
+// console.log({}=={});
+// let a={id:1}
+// a=null
+// console.log(typeof(a));
+
+
+
+
+//practice
+
