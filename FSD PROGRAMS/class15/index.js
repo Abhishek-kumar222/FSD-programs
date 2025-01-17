a6f17b7fe719d4fc7e6357babaaa406fe8f65c25
Abhishek Choudhary <choudharyabhishek40739>
2025-01-17 14:40:26 +0530

    code

---
+++ FSD PROGRAMS/class15/index.js
@@ -0,0 +1,21 @@
+// let str='hello'
+//  str='aaa'
+//  console.log(str);
+ 
+
+// let a=5
+// let b=a++
+// console.log(a+b);
+
+
+// let arr=[1,2,3,4,5,6]
+
+// let sum= arr.reduce((a,b,c,d)=>{
+//     console.log(a+b);
+//     return a+b
+// })
+
+//EC = Exicution contaxt
+// let  a=5
+// console.log(a);
+
