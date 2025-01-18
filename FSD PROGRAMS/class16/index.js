9be20d99b18ea0f234ffb566c4d2e5b671409d0a
Abhishek Choudhary <choudharyabhishek40739>
2025-01-18 19:45:29 +0530

    code

---
+++ FSD PROGRAMS/class16/index.js
@@ -0,0 +1,64 @@
+
+// Hoisting
+
+// console.log(a);
+// const a=5
+
+// var a 
+// console.log(a);
+// a=5
+
+// outer()
+// function outer(){
+//     console.log('helllo');   
+// }
+
+// sum()
+// const sum=()=>{
+//     console.log('hello');   
+// }
+
+// console.log(a);
+// var a=5
+
+// var a 
+// console.log(a);
+// a=5
+
+//     console.log(a);
+//     let a=5
+
+
+//     let a 
+//     console.log(a);
+//     a=5
+
+// let a=5
+
+// console.log(a());
+
+// sum()
+// var sum = ()=>{
+// console.log('helloo');
+
+
+// }
+// let sum=undefined
+// sum()
+// let a=5
+
+// console.log(a());
+// sum()
+// function sum(){
+//     console.log('hello');
+    
+// }
+
+
+// outer()
+// var outer=()=>{
+//     console.log('hiii');
+    
+// }
+
+
